b2910cb6f90ddb7db2aa2dc6465434b3a81b6b73
vennapusa sravanthi <sravanthi@chipweave.com>
2026-09-17 12:21:49 +0530

    Add APB to UART bridge RTL file

---
+++ DSN/ip/peripherals/uart/ip/apbs_with_uart.f
@@ -0,0 +1,31 @@
+# ======================================
+# APB -> UART BRIDGE RTL
+# ======================================
+
+../../../../DSN/ip/peripherals/uart/ip/apb_slave/rtl/uart_apb_top.sv
+
+# ======================================
+# Asynchronous FIFO
+# ======================================
+
+../../../../DSN/ip/peripherals/uart/ip/async_fifo/rtl/async.sv
+../../../../DSN/ip/peripherals/uart/ip/async_fifo/rtl/fifo_mem.sv
+../../../../DSN/ip/peripherals/uart/ip/async_fifo/rtl/rd_ptr.sv
+../../../../DSN/ip/peripherals/uart/ip/async_fifo/rtl/synchronizer.sv
+../../../../DSN/ip/peripherals/uart/ip/async_fifo/rtl/wr_ptr.sv
+
+# ======================================
+# UART
+# ======================================
+
+../../../../DSN/ip/peripherals/uart/ip/uart/rtl/uart_top.sv
+../../../../DSN/ip/peripherals/uart/ip/uart/rtl/uart_tx.sv
+../../../../DSN/ip/peripherals/uart/ip/uart/rtl/uart_rx.sv
+../../../../DSN/ip/peripherals/uart/ip/uart/rtl/uart_fifo.sv
+../../../../DSN/ip/peripherals/uart/ip/uart/rtl/uart_baudgen.sv
+
+# ======================================
+# Reset
+# ======================================
+
+../../../../DSN/ip/peripherals/uart/ip/reset_synchronizer/rtl/reset.sv

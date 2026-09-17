cf360d920dc1ed6e6bc19414402549a0c8a0dd34
vennapusa sravanthi <sravanthi@chipweave.com>
2026-09-17 12:27:20 +0530

    Add synchronizer module for signal synchronization

---
+++ DSN/ip/peripherals/spi/ip/async_fifo/rtl/synchronizer.sv
@@ -0,0 +1,14 @@
+module synchronizer #(parameter WIDTH=3) (input clk, rst_n, [WIDTH:0] d_in, output reg [WIDTH:0] d_out);
+  logic [WIDTH:0] q1;
+  //always@(posedge clk or negedge !rst_n) begin
+  always_ff@(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+      q1 <= 0;
+      d_out <= 0;
+    end
+    else begin
+      q1 <= d_in;
+      d_out <= q1;
+    end
+  end
+endmodule

0c6e01b77396629a3951d79bd646b6c6b184698d
lokeshcw023 <lokesh@chipweave.com>
2026-09-19 12:22:42 +0530

    Update lpddr_wrapper.f

--- DSN/ip/memory_io/lpddr/dsn/rtl/lpddr_wrapper.f
+++ DSN/ip/memory_io/lpddr/dsn/rtl/lpddr_wrapper.f
@@ -1,1 +1,26 @@
+# ==========================================
+# AXI4 -> lpddr Integration RTL
+# ==========================================
 
+$REPO_ROOT/DSN/ip/memory_io/lpddr/dsn/rtl/lpddr5c_soc_wrapper.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/axi_slave/rtl/axi4_slave.sv
+
+# ==========================================
+# lpddr
+# ==========================================
+
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_reg_cfg.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_cmd_scheduler.sv
+$REPO_ROOT/DSN/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_data_path.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_training_ctrl.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_lp_mgmt.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_refresh_ctrl.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_dfi_if.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_top.sv
+
+
+# ==========================================
+# Reset
+# ==========================================
+
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/reset_synchronizer/rtl/reset_synchronizer.sv

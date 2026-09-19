a5cf8f43dafbb6222dcdef9b591358d24a0400b1
lokeshcw023 <lokesh@chipweave.com>
2026-09-19 10:17:36 +0530

    Update async.f

--- DSN/ip/memory_io/usb/ip/async_fifo/rtl/async.f
+++ DSN/ip/memory_io/usb/ip/async_fifo/rtl/async.f
@@ -1,1 +1,9 @@
+# ==========================================
+# Asynchronous FIFO
+# ==========================================
 
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/async.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/fifo_mem.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/wptr_handler.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/rptr_handler.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/synchronizer.sv

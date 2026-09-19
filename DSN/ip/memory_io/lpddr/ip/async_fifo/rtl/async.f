a88015a97b26216221e5bf32770c57787ee115b3
lokeshcw023 <lokesh@chipweave.com>
2026-09-19 10:40:58 +0530

    Update async.f

--- DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/async.f
+++ DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/async.f
@@ -2,8 +2,8 @@
 # Asynchronous FIFO
 # ==========================================
 
-$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/async.sv
-$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/fifo_mem.sv
-$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/wptr_handler.sv
-$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/rptr_handler.sv
-$REPO_ROOT/DSN/ip/memory_io/usb/ip/async_fifo/rtl/synchronizer.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/async.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/fifo_mem.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/wptr_handler.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/rptr_handler.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/synchronizer.sv

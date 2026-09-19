21dc764508c93fbe51cdb176baa54ddb94b8cbb5
lokeshcw023 <lokesh@chipweave.com>
2026-09-19 10:13:58 +0530

    Update rptr_handler.sv

--- DSN/ip/memory_io/usb/ip/async_fifo/rtl/rptr_handler.sv
+++ DSN/ip/memory_io/usb/ip/async_fifo/rtl/rptr_handler.sv
@@ -1,1 +1,36 @@
+`timescale 1ns/1ps
+
+module rptr_handler #(
+    parameter integer PTR_WIDTH = 3
+)(
+    input                 rclk,
+    input                 rrst_n,
+    input                 r_en,
+    input   [PTR_WIDTH:0] g_wptr_sync,
+    output reg  [PTR_WIDTH:0] b_rptr,
+    output reg  [PTR_WIDTH:0] g_rptr,
+    output reg                empty
+);
+
+    reg [PTR_WIDTH:0] b_rptr_next;
+    reg [PTR_WIDTH:0] g_rptr_next;
+    
+
+    assign b_rptr_next = b_rptr + (r_en && !empty);
+    assign g_rptr_next = (b_rptr_next >> 1) ^ b_rptr_next;
+    assign empty_next  = (g_wptr_sync == g_rptr_next);
+
+    always @(posedge rclk or negedge rrst_n) begin
+        if (!rrst_n) begin
+            b_rptr <= '0;
+            g_rptr <= '0;
+            empty  <= 1'b1;
+        end else begin
+            b_rptr <= b_rptr_next;
+            g_rptr <= g_rptr_next;
+            empty  <= empty_next;
+        end
+    end
+
+endmodule
 

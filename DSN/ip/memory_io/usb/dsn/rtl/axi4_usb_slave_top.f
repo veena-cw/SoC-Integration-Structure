f3ae01422d82330032a0fc4ceb43713c857bc39c
lokeshcw023 <lokesh@chipweave.com>
2026-09-19 10:50:11 +0530

    Update axi4_usb_slave_top.f

--- DSN/ip/memory_io/usb/dsn/rtl/axi4_usb_slave_top.f
+++ DSN/ip/memory_io/usb/dsn/rtl/axi4_usb_slave_top.f
@@ -1,1 +1,28 @@
+# ==========================================
+# APB -> I2C BRIDGE RTL
+# ==========================================
 
+$REPO_ROOT/DSN/ip/peripherals/i2c/dsn/rtl/i2c_top.sv
+$REPO_ROOT/DSN/ip/peripherals/i2c/ip/apb_slave/rtl/apb_slave.sv
+
+
+# ==========================================
+# I2C
+# ==========================================
+
+$REPO_ROOT/DSN/ip/peripherals/i2c/ip/i2c_master/rtl/i2c_master.sv
+
+
+# ==========================================
+# Reset
+# ==========================================
+
+$REPO_ROOT/DSN/ip/peripherals/i2c/ip/reset_synchronizer/rtl/reset_synchronizer.sv
+
+
+# ==========================================
+# Asynchronous FIFO File List
+# ==========================================
+
+
+-f $REPO_ROOT/DSN/ip/peripherals/i2c/ip/async_fifo/rtl/async.f

f2fafd26668cf1a8fb3e38ca44edb97f5424999a
lokeshcw023 <lokesh@chipweave.com>
2026-09-19 10:42:14 +0530

    Delete DSN/ip/memory_io/lpddr/ip/async_fifo/rtl directory

--- DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/async.f
+++
@@ -1,9 +0,0 @@
-# ==========================================
-# Asynchronous FIFO
-# ==========================================
-
-$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/async.sv
-$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/fifo_mem.sv
-$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/wptr_handler.sv
-$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/rptr_handler.sv
-$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/async_fifo/rtl/synchronizer.sv

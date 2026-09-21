3e4d2e97b8b7a2404b86a8c8a3565621371b75e9
lokeshcw023 <lokesh@chipweave.com>
2026-09-21 14:52:49 +0530

    Update usb.f

--- DSN/ip/memory_io/usb/ip/usb/rtl/usb.f
+++ DSN/ip/memory_io/usb/ip/usb/rtl/usb.f
@@ -1,1 +1,10 @@
+# ==========================================
+# usb
+# ==========================================
 
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/usb/rtl/usbh_host_defs.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/usb/rtl/usbh_fifo.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/usb/rtl/usbh_crc5.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/usb/rtl/usbh_crc16.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/usb/rtl/usbh_sie.sv
+$REPO_ROOT/DSN/ip/memory_io/usb/ip/usb/rtl/usbh_host.sv

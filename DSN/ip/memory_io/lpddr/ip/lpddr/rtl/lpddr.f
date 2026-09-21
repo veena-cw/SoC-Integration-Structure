052857a8f48d744ba66fdf4472772612d4f52cb6
lokeshcw023 <lokesh@chipweave.com>
2026-09-21 14:53:38 +0530

    Update lpddr.f

--- DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr.f
+++ DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr.f
@@ -1,1 +1,12 @@
+# ==========================================
+# lpddr
+# ==========================================
 
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_reg_cfg.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_cmd_scheduler.sv
+$REPO_ROOT/DSN/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_data_path.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_training_ctrl.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_lp_mgmt.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_refresh_ctrl.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_dfi_if.sv
+$REPO_ROOT/DSN/ip/memory_io/lpddr/ip/lpddr/rtl/lpddr5c_top.sv

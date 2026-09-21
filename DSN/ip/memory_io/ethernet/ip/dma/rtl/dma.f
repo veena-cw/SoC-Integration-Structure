856fe1af84faea3c87150edc4e478efcc20da094
lokeshcw023 <lokesh@chipweave.com>
2026-09-21 14:55:27 +0530

    Update dma.f

--- DSN/ip/memory_io/ethernet/ip/dma/rtl/dma.f
+++ DSN/ip/memory_io/ethernet/ip/dma/rtl/dma.f
@@ -1,6 +1,10 @@
-taxi_axi_dma.sv
-taxi_axi_dma_wr.sv
-taxi_axi_dma_rd.sv
-taxi_dma_desc_if.sv
-../lib/taxi/src/axis/rtl/taxi_axis_if.sv
-../lib/taxi/src/axi/rtl/taxi_axi_if.sv
+# ==========================================
+# dma
+# ==========================================
+
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_dma.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_dma_wr.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_dma_rd.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_dma_desc_if.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axis_if.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_if.sv

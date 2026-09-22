739e257081f7821290107ea9fd916179896da09e
Jishnu Mohan <cw025_jishnu@ip-10-0-10-79.ap-south-2.compute.internal>
2026-09-22 17:28:55 +0000

    Added sram IP file list

---
+++ DSN/ip/compute/sram/ip/sram/rtl/sram.f
@@ -0,0 +1,8 @@
+# ==========================================
+# SRAM
+# ==========================================
+
+$REPO_ROOT/DSN/ip/compute/sram/ip/sram/rtl/tc_clk.sv
+$REPO_ROOT/DSN/ip/compute/sram/ip/sram/rtl/tc_sram_impl.sv
+$REPO_ROOT/DSN/ip/compute/sram/ip/sram/rtl/tc_sram.sv
+$REPO_ROOT/DSN/ip/compute/sram/ip/sram/rtl/tc_sync.sv

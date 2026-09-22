3a99026380cf24c30480151035a0da3e89662fac
Jishnu Mohan <cw025_jishnu@ip-10-0-10-79.ap-south-2.compute.internal>
2026-09-22 17:01:10 +0000

    Added plic IP file list

---
+++ DSN/ip/compute/plic/ip/plic/rtl/plic.f
@@ -0,0 +1,11 @@
+# ==========================================
+# PLIC
+# ==========================================
+
+$REPO_ROOT/DSN/ip/compute/plic/ip/plic/rtl/verilog/ahb3lite/ahb3lite_plic_top.sv
+$REPO_ROOT/DSN/ip/compute/plic/ip/plic/rtl/verilog/core/plic_cell.sv
+$REPO_ROOT/DSN/ip/compute/plic/ip/plic/rtl/verilog/core/plic_core.sv
+$REPO_ROOT/DSN/ip/compute/plic/ip/plic/rtl/verilog/core/plic_dynamic_registers.sv
+$REPO_ROOT/DSN/ip/compute/plic/ip/plic/rtl/verilog/core/plic_gateway.sv
+$REPO_ROOT/DSN/ip/compute/plic/ip/plic/rtl/verilog/core/plic_priority_index.sv
+$REPO_ROOT/DSN/ip/compute/plic/ip/plic/rtl/verilog/core/plic_target.sv

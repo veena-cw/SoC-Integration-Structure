da6424b146458420d12e1680b2b6bf530b7c7123
Jishnu Mohan <cw025_jishnu@ip-10-0-10-79.ap-south-2.compute.internal>
2026-09-22 15:56:15 +0000

    Added cpu top flist

--- DSN/ip/compute/cpu/dsn/rtl/cpu_top.f
+++ DSN/ip/compute/cpu/dsn/rtl/cpu_top.f
@@ -1,1 +1,37 @@
+# ============================================================
+# BlackParrot CPU RTL
+# ============================================================
+-f $REPO_ROOT/DSN/ip/compute/cpu/ip/cpu/rtl/cpu.f
 
+
+# ============================================================
+# Async FIFO include directory
+# Required because bp_bedrock_axi4_bridge.sv contains:
+# `include "async.sv"
+# ============================================================
++incdir+$REPO_ROOT/DSN/ip/compute/cpu/ip/async_fifo/rtl
+
+
+# ============================================================
+# Async FIFO RTL
+# ============================================================
+$REPO_ROOT/DSN/ip/compute/cpu/ip/async_fifo/rtl/async.sv
+
+
+
+# ============================================================
+# AXI4 Master
+# ============================================================
+$REPO_ROOT/DSN/ip/compute/cpu/ip/axi4_master/rtl/axi4_master.sv
+
+
+# ============================================================
+# BedRock -> AXI4 Bridge
+# ============================================================
+$REPO_ROOT/DSN/ip/compute/cpu/dsn/rtl/bp_cpu_axi4m_if/bp_bedrock_axi4_bridge.sv
+
+
+# ============================================================
+# Top Module
+# ============================================================
+$REPO_ROOT/DSN/ip/compute/cpu/dsn/rtl/bp_cpu_axi4m_if/bp_bedrock_axi4_soc_top.sv

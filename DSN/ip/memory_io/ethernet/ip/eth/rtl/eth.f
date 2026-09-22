d2f99bc46ff0f190cf728c0e88d0f223849f9ce3
lokeshcw023 <lokesh@chipweave.com>
2026-09-22 10:10:38 +0530

    Update eth.f

--- DSN/ip/memory_io/ethernet/ip/eth/rtl/eth.f
+++ DSN/ip/memory_io/ethernet/ip/eth/rtl/eth.f
@@ -12,7 +12,6 @@ $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_sync_reset.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_sync_signal.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_if.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_arb_mux.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_if.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_arbiter.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_penc.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_stats_collect.sv
@@ -21,19 +20,12 @@ $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_mac_ctrl_rx.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_mac_pause_ctrl_tx.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_mac_pause_ctrl_rx.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_lfsr.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_if.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_pad.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_null_src.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_tie.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_sync_signal.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_rgmii_phy_if.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_ssio_ddr_in.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_iddr.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_oddr.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_sync_reset.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_async_fifo_adapter.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_async_fifo.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_sync_reset.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_sync_signal.sv
-$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_if.sv
 $REPO_ROOT/DSN/ip/memory_io/ethernet/ip/eth/rtl/taxi_axis_adapter.sv

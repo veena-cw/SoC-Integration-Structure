4bfaf74d34edae514f175c8752cebdc1511999de
lokeshcw023 <lokesh@chipweave.com>
2026-09-22 16:01:31 +0530

    Update eth_top.f

--- DSN/ip/memory_io/ethernet/dsn/rtl/eth_top.f
+++ DSN/ip/memory_io/ethernet/dsn/rtl/eth_top.f
@@ -5,6 +5,17 @@
 $REPO_ROOT/DSN/ip/memory_io/ethernet/dsn/rtl/eth_top.sv
 
 # ==========================================
+# dma
+# ==========================================
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_dma.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_dma_wr.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_dma_rd.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_dma_desc_if.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axis_if.sv
+$REPO_ROOT/DSN/ip/memory_io/ethernet/ip/dma/rtl/taxi_axi_if.sv
+
+
+# ==========================================
 # ethernet
 # ==========================================
 
